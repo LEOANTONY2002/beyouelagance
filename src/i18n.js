55416b8040d7d941bd79053a20a7277b84bb2ae5
Leo Antony <leo.antony@changecx.com>
2024-04-18 17:59:40 +0530

    i18

--- src/i18n.js
+++ src/i18n.js
@@ -39,11 +39,11 @@ const resources = {
       "HAIR_CARE": "Hair Care",
       "NAIL_CARE": "Nail Care",
       "SPA_CARE": "Spa Care",
-      "BRIDAL_DESC": "The glow a bride gives off after receiving bridal treatments is simply dazzling. This allows you to obtain radiant skin, relieves stress, enhances beauty, builds self-confidence, encourages self-love and offers a moment of personal reflection.",
-      "FACE_DESC": "A gentle touch for your face, which will make your skin glow while Preventing signs of aging and acne marks. I allowing it to bloom like a beautiful flower",
-      "HAIR_DESC": "Hair care is important for several reasons. Take care of your hair helps prevent damage and breakage. By using the vouchers products and techniques You can Achieve texture, shine and style desired. Hair care can improve the appearance of your hair and increase your self-confidence. Invest in your Hair They are the crown that you never remove.",
-      "NAIL_DESC": "Proper nail care can help prevent problems like such as brittle nails, hung skin and fungal infections. In doing a manicure, it gives an improved appearance and better nail health a little self for. Your nails are made sparkling. Your Nails are jewelry not tools.",
-      "SPA_DESC": "Spa treatments can help reduce stress, improve circulation Sanguine and promote muscle relaxation. It's a real moment of pleasure to recharge your batteries and feel good about yourself.",
+      "BRIDAL_CARE_DESC": "The glow a bride gives off after receiving bridal treatments is simply dazzling. This allows you to obtain radiant skin, relieves stress, enhances beauty, builds self-confidence, encourages self-love and offers a moment of personal reflection.",
+      "FACE_CARE_DESC": "A gentle touch for your face, which will make your skin glow while Preventing signs of aging and acne marks. I allowing it to bloom like a beautiful flower",
+      "HAIR_CARE_DESC": "Hair care is important for several reasons. Take care of your hair helps prevent damage and breakage. By using the vouchers products and techniques You can Achieve texture, shine and style desired. Hair care can improve the appearance of your hair and increase your self-confidence. Invest in your Hair They are the crown that you never remove.",
+      "NAIL_CARE_DESC": "Proper nail care can help prevent problems like such as brittle nails, hung skin and fungal infections. In doing a manicure, it gives an improved appearance and better nail health a little self for. Your nails are made sparkling. Your Nails are jewelry not tools.",
+      "SPA_CARE_DESC": "Spa treatments can help reduce stress, improve circulation Sanguine and promote muscle relaxation. It's a real moment of pleasure to recharge your batteries and feel good about yourself.",
     }
   },
   fr: {
@@ -82,11 +82,11 @@ const resources = {
       "HAIR_CARE": "Soins Capillaires",
       "NAIL_CARE": "Soins des ongles ",
       "SPA_CARE": "Soins en spa",
-      "BRIDAL_DESC": "L'éclat que dégage une mariée après avoir reçu des soins de mariée est simplement éblouissante. Cela permet d'obtenir un peau éclatante, soulage la stress, améliore la beauté, renforce la confiance en soi, encourage l'amour de soi et offre un moment de réflexion personnelle.",
-      "FACE_DESC": "Un doux effleurement pour ton visage, qui à faire rayonner ta peau tout en prévenant signes de les signes de vieillissement et les marques d'acné. Je permettant ainsi d'écolore telle une belle fleur",
-      "HAIR_DESC": "Les soins capillaires sont importants pour plusieurs raisons. Prendre soin de ses cheveux permet de prévenir les dommages et la casse. En utilisant les bons produits et techniques Vous pouvez Obtenir la texture, la brillance et le style désirés. Le soins capillaires peuvent ameliorer 1'apparence de vos cheveux et accroître votre Confiance en vous. Investissez dans vos Cheveux Ils sont la couronne que vous ne retirez jamais.",
-      "NAIL_DESC": "Les soins adéquats des ongles peuvent aider à prévenir des problémes tels que les ongles cassants, les peaux accrochées et les infections fongiques. En faisant une manucure, cela donne une apparence améliorée et une meilleure santé des ongles un peu des sois pour. Vos ongles sont les rend étencilants. Vos ongles sont bijoux pas des outils.",
-      "SPA_DESC": "Les soins en spa peuvent aider à réduire le stress, ameliorer la circulation Sanguine et favoriser la relaxation musculaire. C'est un véritable moment de plaisir рour se resourcer et se sentir bien dans sa peau.",
+      "BRIDAL_CARE_DESC": "L'éclat que dégage une mariée après avoir reçu des soins de mariée est simplement éblouissante. Cela permet d'obtenir un peau éclatante, soulage la stress, améliore la beauté, renforce la confiance en soi, encourage l'amour de soi et offre un moment de réflexion personnelle.",
+      "FACE_CARE_DESC": "Un doux effleurement pour ton visage, qui à faire rayonner ta peau tout en prévenant signes de les signes de vieillissement et les marques d'acné. Je permettant ainsi d'écolore telle une belle fleur",
+      "HAIR_CARE_DESC": "Les soins capillaires sont importants pour plusieurs raisons. Prendre soin de ses cheveux permet de prévenir les dommages et la casse. En utilisant les bons produits et techniques Vous pouvez Obtenir la texture, la brillance et le style désirés. Le soins capillaires peuvent ameliorer 1'apparence de vos cheveux et accroître votre Confiance en vous. Investissez dans vos Cheveux Ils sont la couronne que vous ne retirez jamais.",
+      "NAIL_CARE_DESC": "Les soins adéquats des ongles peuvent aider à prévenir des problémes tels que les ongles cassants, les peaux accrochées et les infections fongiques. En faisant une manucure, cela donne une apparence améliorée et une meilleure santé des ongles un peu des sois pour. Vos ongles sont les rend étencilants. Vos ongles sont bijoux pas des outils.",
+      "SPA_CARE_DESC": "Les soins en spa peuvent aider à réduire le stress, ameliorer la circulation Sanguine et favoriser la relaxation musculaire. C'est un véritable moment de plaisir рour se resourcer et se sentir bien dans sa peau.",
     }
   }
 };
